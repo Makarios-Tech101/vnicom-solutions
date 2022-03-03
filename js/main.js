02c004400871215b2c9d17d0935d340a94328463
Ohien Stephen <ohienstephen@gmail.com>
2022-03-03 20:25:53 +0100

    ---------

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
-menu = document.getElementsByClassName("menu")[0]
-menu_bar = document.getElementById("menu-bar")
+window.addEventListener('load', ()=>{
+    const menu = document.getElementsByClassName("menu")[0]
+    const menu_bar = document.getElementById("menu-bar")
+    const toggle = document.getElementsByClassName("toggle")
 
-menu_bar.addEventListener('click', ()=>{
-    menu.classList.toggle('show')
+    console.log(toggle)
+
+    Array.from(toggle).forEach(element => {
+        element.addEventListener('click', toggle_menu)
+    });
+
+    menu_bar.addEventListener('click', toggle_menu)
+
+    function toggle_menu(){
+        menu.classList.toggle('show')
+    }
 })
